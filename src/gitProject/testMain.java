ce7ae781212dbd712aa9503fdd6ea727858dd5c9
zy <devil.index@163.com>
2020-02-26 23:43:36 +0800

    初始化git项目

---
+++ src/gitProject/testMain.java
@@ -0,0 +1,10 @@
+package gitProject;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word to github - 20200226");
+	}
+
+}
